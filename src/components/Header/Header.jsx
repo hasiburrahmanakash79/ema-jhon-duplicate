41d87320d178f7a09dfab7e918ab87a265560d15
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-04-05 23:33:38 +0600

    add router in Home section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/home">Home</a>
+                <a href="/">Home</a>
                 <a href="/order">Order</a>
                 <a href="/order-review">Order Review</a>
                 <a href="/inventory">Manage Inventory</a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Shop from './components/Shop/Shop';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
